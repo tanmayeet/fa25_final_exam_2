ac41bb0eb17481ef8d81953262dcaf1b3daf3175
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-12-11 16:19:27 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -9,7 +9,34 @@ struct Node {
   Node* next;
 };
 
-// sim
+// adds customer to end of the queue
+// referenced notes on linked lists while writing this
+void enqueue(Node*& head, Node*& tail, string n, string d) {
+  Node* temp = new Node;
+  temp->name = n;
+  temp->drink = d;
+  temp->next = nullptr;
+
+  if (!head) {
+    head = tail = temp;
+  } else {
+    tail->next = temp;
+    tail = temp;
+  }
+}
+
+void deque(Node*& head, Node*& tail) {
+  if (!head) {
+    return;
+  }
+  Node* temp = head;
+  head = head->next;
+
+  if (!head) {
+    tail = nullptr;
+  }
+  delete temp;
+}
 
 int main() {
   string names[] = {"Alex", "Jordan", "Taylor", "Morgan", "Casey"};
